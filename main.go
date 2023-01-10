2aa0ef119dc0ab85bc5c64bfe675fff560ab9f00
Danny-Co <daaniel.uriarte@gmail.com>
2023-01-09 23:07:13 -0800

    command line args

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func findos() {
+	opsystem = runtime.GOOS
+	fmt.Print(opsystem, "\n")
+}
+
+func command() {
+	cmd := exec.Command("ls", "./")
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("could not run command: ", err)
+	} else {
+		fmt.Println("Output: ", string(out))
+	}
+}
+
 func main() {
 	findos()
+	command()
 }
--- setup.go
+++ setup.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 	"runtime"
 )
 
@@ -9,5 +10,15 @@ var opsystem string
 
 func findos() {
 	opsystem = runtime.GOOS
-	fmt.Print(opsystem)
+	fmt.Print(opsystem, "\n")
+}
+
+func command() {
+	cmd := exec.Command("ls", "./")
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("could not run command: ", err)
+	} else {
+		fmt.Println("Output: ", string(out))
+	}
 }
